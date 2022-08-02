cb6986cebf92c64553de2399dbc32e7567e30f59
Kacper Grzeszczyk <kacgrzes@gmail.com>
2022-08-02 12:39:11 +0200

    Fix typo on maintenance-on script

--- maintenance-on.sh
+++ maintenance-on.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '{"status": "DOWN", "message":"Application is currrenlty in maintenance mode"}' > status.json
+echo '{"status": "DOWN", "message":"Application is currently in maintenance mode"}' > status.json
 git add status.json
 git commit -am "maintenance on"
 git push -f origin main
